c77b220002ec2b1a571ac2fa4f4948b25326921c
Safin-Ali <safin.ali.7205@gmail.com>
2022-11-07 23:48:38 +0600

    dotenv,react-router,tailwind css, navbar component,home page, routes created

---
+++ src/Routes/Routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+
+const router = createBrowserRouter([
+    {path: '/', element: <Home></Home>}
+])
+
+export default router;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        {/* Brand Logo */}
+        <div>
+            <img src="" alt="" />
+        </div>
+
+        {/* Route NavLink */}
+        <div>
+        </div>
+
+        {/* User Profile */}
+        <div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
