a1898620869f3f1c6c71944b5038a13fdf3427e7
Safin-Ali <safin.ali.7205@gmail.com>
2022-11-09 05:17:36 +0600

    firebase env variable created and uninstalled react carousel

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBI-MlDaPVywf2Guv4xBedhUd8jPDndkys",
-  authDomain: "a-accountant.firebaseapp.com",
-  projectId: "a-accountant",
-  storageBucket: "a-accountant.appspot.com",
-  messagingSenderId: "426416198144",
-  appId: "1:426416198144:web:37b77851f6698a9a0d7ee5"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
