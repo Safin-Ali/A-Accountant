6ed6f8ff646e70f24297d73f9a185e1a684413d3
Safin-Ali <safin.ali.7205@gmail.com>
2022-11-10 19:04:07 +0600

    review form responsived

--- src/components/form/ReviewForm.js
+++ src/components/form/ReviewForm.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import {MdRateReview} from 'react-icons/md';
 import {GrClose} from 'react-icons/gr';
 import { AuthData } from '../../context/AuthContext';
 import { Link } from 'react-router-dom';
